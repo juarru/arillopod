9c36213dff79e9bf156b7e7b36e59d739fcab76c
Juan Arillo <juan.arillo@ilusia.com>
2018-10-19 12:21:11 +0200

    Modified files

--- app.js
+++ app.js
@@ -5,40 +5,51 @@ var request = require('request');
 var fs = require('fs');
 const csv=require('csvtojson');
 
-// var source1 = get_mongo_values();
+get_mongo_values(function () {
+	console.log(rest);
+});
 // get_api_values().then(save_api_values);
 
-const csvFilePath='./csv/api_data.csv';
-csv().fromFile(csvFilePath).then((jsonObject) => {
-	var i = 0;
-	for(record in jsonObject){
-		i = i+1;
-		console.log(i);
-	}
-});
+// const csvFilePath='./csv/api_data.csv';
+// csv().fromFile(csvFilePath).then((jsonObject) => {
+// 	var i = 0;
+// 	for(record in jsonObject){
+// 		i = i+1;
+// 		console.log(i);
+// 	}
+// });
 
 
 /* ---- Auxiliar functions ---- */
-function get_mongo_values() {
+function get_mongo_values(callback) {
 	// Connecting to MongoDB and retrieving data.
 	var url = "mongodb://" + process.env.MG_USER + ":" + process.env.MG_PWD + "@" + process.env.MG_HOST + ":" + process.env.MG_PORT + "/" + process.env.MG_DATABASE;
 	console.log(url);
 	MongoClient.connect(url, { useNewUrlParser: true },  function (err, db) {
 		if(!err){
 			console.log("Connection OK");
+			// Getting collection data
+			try {
+				var dbo = db.db(process.env.MG_DATABASE);
+				var rest = [];
+				var mongo_data = dbo.collection(process.env.MG_COLLECTION).find({'call.startTime': {$gte: '2018-05-01 00:00:00', $lte: '2018-05-01 23:59:59' }, 'call.endTime' : {$gte: '2018-05-01 00:00:00', $lte: '2018-05-01 23:59:59'}})
+					.toArray(function(err, result) {
+						if (err) {
+							console.log(err);
+						} else if (result.length > 0) {
+							rest.push(result);
+							callback();
+						}
+					});
+				console.log(mongo_data);
+				return mongo_data;
+			} catch (e) {
+				console.log(e);
+			}
+			db.close();
 		} else {
 			console.log(err);
 		}
-
-		// Getting collection data
-        try {
-			var dbo = db.db(process.env.MG_DATABASE);
-			var mongo_data = dbo.collection(process.env.MG_COLLECTION).find({'call.startTime': {$gte: '2018-05-01 00:00:00', $lte: '2018-05-01 23:59:59' }, 'call.endTime' : {$gte: '2018-05-01 00:00:00', $lte: '2018-05-01 23:59:59'}}).toArray();
-			return(mongo_data);
-        } catch (e) {
-            console.log(e);
-		}
-		db.close();
 	});
 }
 
